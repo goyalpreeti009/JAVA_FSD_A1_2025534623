81917b66c8373a590081d7114596cd4893c090c4
GoyalPreeti <goyalpreeti009@gamil.com>
2026-08-12 00:43:19 +0530

    po3

---
+++ PRACTICALS/P03_SwapWithoutThirdVar.java
@@ -0,0 +1,25 @@
+package PRACTICALS;
+import java.util.Scanner;
+
+public class P03_SwapWithoutThirdVar {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter b: ");
+        int b = scanner.nextInt();
+
+        System.out.println("Before: a = " + a + ", b = " + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After : a = " + a + ", b = " + b);
+
+        scanner.close();
+    
+    }
+    
+}
