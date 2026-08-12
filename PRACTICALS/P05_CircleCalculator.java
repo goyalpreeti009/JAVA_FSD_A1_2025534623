61ae89463a9e33cef8c3af4d1016f44df0837319
GoyalPreeti <goyalpreeti009@gamil.com>
2026-08-12 14:34:30 +0530

    P05

---
+++ PRACTICALS/P05_CircleCalculator.java
@@ -0,0 +1,19 @@
+package PRACTICALS;
+
+import java.util.Scanner;
+
+public class P05_CircleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter radius");
+        double radius = sc.nextDouble();
+         double area = Math.PI * radius * radius;
+        double circumference = 2 * Math.PI * radius;
+
+        System.out.printf("Area          = %.2f%n", area);
+        System.out.printf("Circumference = %.2f%n", circumference);
+
+        sc.close();
+    }
+    
+}
