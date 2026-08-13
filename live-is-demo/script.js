7c4afd0efaae95dfc0931a7a366362bc271f68ae
GoyalPreeti <goyalpreeti009@gamil.com>
2026-08-13 14:21:25 +0530

    editted

--- live-is-demo/script.js
+++ live-is-demo/script.js
@@ -1,12 +1,22 @@
 const moodBtn = document.getElementById("moodBtn");
 
 moodBtn.addEventListener("click", () => {
-    alert("Button clicked!");
+    console.log("Button clicked!");
 });
 
 const moods = [
     {color: "#FFE5B4" , text: "Feeling Peachy"},
-    { color: "#B4E!FF" , text: "Feeling Cool & Calm"},
+    { color: "#B4E1FF" , text: "Feeling Cool & Calm"},
     {color: "#D8B4FF" , text: "Feeling Mysterious"},
-    {color: "#B4FFC1" , text: "Feeling Freash"},
-]
+    {color: "#B4FFC1" , text: "Feeling Fresh"},
+    {color: "#FFB4B4" , text: "Feeling Fired Up"},
+];
+
+const moodText = document.getElementById("moodText");
+moodBtn.addEventListener("click",() =>  {
+    const randomIndex = Math.floor(Math.random() *moods.length);
+    const chosenMood = moods[randomIndex];
+
+    document.body.style.backgroundColor = chosenMood.color;
+    moodText.textContent = chosenMood.text;
+});
