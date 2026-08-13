51e619bb18914d9e3ce827d1b5b2dff7dc6faf82
GoyalPreeti <goyalpreeti009@gamil.com>
2026-08-13 14:03:10 +0530

    chngd

---
+++ live-is-demo/script.js
@@ -0,0 +1,12 @@
+const moodBtn = document.getElementById("moodBtn");
+
+moodBtn.addEventListener("click", () => {
+    alert("Button clicked!");
+});
+
+const moods = [
+    {color: "#FFE5B4" , text: "Feeling Peachy"},
+    { color: "#B4E!FF" , text: "Feeling Cool & Calm"},
+    {color: "#D8B4FF" , text: "Feeling Mysterious"},
+    {color: "#B4FFC1" , text: "Feeling Freash"},
+]
